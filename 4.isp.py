b72abfecf16785c166ba59992e85aa6c9c4253fe
EricAlex <ericxl2002@gmail.com>
2023-05-04 22:19:55 -0300

    Adicionei 4 e 5

--- 4.isp.py
+++ 4.isp.py
@@ -2,28 +2,39 @@
 Interface Segregation Principle
 Crie interfaces que são específicas. Clientes não devem depender de interfaces que eles não usarão
 """
+
+'''
+class Player:
+    def __init__(self, name):
+        self.stats = StatsReporter(self)
+        self.__name = name
+        self.__hp = 100
+        self.__speed = 1
+
+    def hp(self):
+        return self.__hp
+
+    def name(self):
+        return self.__name
+
+class StatsReporter:
+    def __init__(self, char: Player):
+        self.char = char
+
+    def report(self):
+        print(f'Name:{self.char.name()}')
+        print(f'HP:{self.char.hp()}')
+'''
 from abc import ABC
 
 class IJanela(ABC):
-    def maximizar(self):
-        raise NotImplementedError
-    
     def minimizar(self):
         raise NotImplementedError
-    
-    def mostrar_menu(self):
-        raise NotImplementedError
-        
+
     def fechar(self):
         raise NotImplementedError
 
 class JanelaTamanhoFixo(IJanela):
-    def maximizar(self):
-        pass
-
-    def minimizar(self):
-        pass
-    
     def mostrar_menu(self):
         pass
     
@@ -32,15 +43,7 @@ class JanelaTamanhoFixo(IJanela):
 
 class JanelaSemMenu(IJanela):
     def maximizar(self):
-        pass
+        raise NotImplementedError
 
-    def minimizar(self):
-        pass
-    
-    def mostrar_menu(self):
-        pass
-    
-    def fechar(self):
-        pass
 
 
--- 5.dip.py
+++ 5.dip.py
@@ -4,8 +4,7 @@ Dependency Inversion Principle
 Dependências devem ser feitas sobre abstrações, não sobre implementações concretas 
 
 """
-
-
+'''
 class Player:
     def __init__(self, name):
         self.stats = StatsReporter(self)
@@ -26,3 +25,34 @@ class StatsReporter:
     def report(self):
         print(f'Name:{self.char.name()}')
         print(f'HP:{self.char.hp()}')
+'''
+class Entidade:
+    def __init__(self, name, hp, speed):
+        self.stats = StatsReporter(self)
+        self.__name = name
+        self.__hp = hp
+        self.__speed = speed
+
+    def hp(self):
+        return self.__hp
+
+    def name(self):
+        return self.__name
+
+    def speed(self):
+        return self.__speed
+
+class Player(Entidade):
+    def __init__(self, name):
+        super().__init__(name, 100, 1)
+
+class StatsReporter:
+    def __init__(self, char: Entidade):
+        self.char = char
+
+    def report(self):
+        print(f'Name:{self.char.name()}')
+        print(f'HP:{self.char.hp()}')
+
+p1 = Player("p1")
+p1.stats.report()
